e5bd74e9af601129af3cf57e354a6072675dfa0c
Tellili <tellilihaithem@gmail.com>
2023-12-14 11:02:35 +0000

    added custom exp files

--- exps/example/custom/yolox_m.py
+++ exps/example/custom/yolox_m.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 # Copyright (c) Megvii, Inc. and its affiliates.
+
 import os
 
 from yolox.exp import Exp as MyExp
@@ -9,16 +10,18 @@ from yolox.exp import Exp as MyExp
 class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
-        self.depth = 0.33
-        self.width = 0.50
+        self.depth = 0.67
+        self.width = 0.75
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
 
-        # Define yourself dataset path
+
         self.data_dir = "datasets/3k_dataset"
         self.train_ann = "train.json"
         self.val_ann = "val.json"
         self.num_classes = 10
 
-        self.max_epoch = 10
+        self.max_epoch = 15
         self.data_num_workers = 4
         self.eval_interval = 1
+        self.input_size = (800, 800)
+        self.test_size = (800, 800)
